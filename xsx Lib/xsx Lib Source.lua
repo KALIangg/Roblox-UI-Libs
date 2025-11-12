84520374a00ffd44d6a6550973a0551a9d524259
DogScripts <strongbear7980@gmail.com>
2025-11-12 17:19:25 -0300

    Update xsx Lib Source.lua

--- xsx Lib/xsx Lib Source.lua
+++ xsx Lib/xsx Lib Source.lua
@@ -758,8 +758,8 @@ function library:Introduction()
     xsxLogo.Position = UDim2.new(0.5, 0, 0.5, 0)
     xsxLogo.Size = UDim2.new(0, 448, 0, 150)
     xsxLogo.Visible = true
-    xsxLogo.Image = "http://www.roblox.com/asset/?id=9365068051"
-    xsxLogo.ImageColor3 = Color3.fromRGB(159, 115, 255)
+    xsxLogo.Image = "http://www.roblox.com/asset/?id=281338498"
+    xsxLogo.ImageColor3 = Color3.fromRGB(255, 0, 0)
     xsxLogo.ImageTransparency = 1
     
     hashLogo.Name = "hashLogo"
@@ -770,7 +770,7 @@ function library:Introduction()
     hashLogo.Position = UDim2.new(0.5, 0, 0.5, 0)
     hashLogo.Size = UDim2.new(0, 150, 0, 150)
     hashLogo.Visible = true
-    hashLogo.Image = "http://www.roblox.com/asset/?id=9365069861"
+    hashLogo.Image = "http://www.roblox.com/asset/?id=112280163844400"
     hashLogo.ImageColor3 = Color3.fromRGB(159, 115, 255)
     hashLogo.ImageTransparency = 1
     
@@ -780,8 +780,8 @@ function library:Introduction()
     xsx.BackgroundTransparency = 1.000
     xsx.Size = UDim2.new(0, 80, 0, 21)
     xsx.Font = Enum.Font.Code
-    xsx.Text = "powered by xsx"
-    xsx.TextColor3 = Color3.fromRGB(124, 124, 124)
+    xsx.Text = "powered by Poze"
+    xsx.TextColor3 = Color3.fromRGB(255, 0, 0)
     xsx.TextSize = 10.000
     xsx.TextTransparency = 1
     
@@ -792,7 +792,7 @@ function library:Introduction()
     text.Position = UDim2.new(0.912751675, 0, 0, 0)
     text.Size = UDim2.new(0, 26, 0, 21)
     text.Font = Enum.Font.Code
-    text.Text = "hash"
+    text.Text = "Kali"
     text.TextColor3 = Color3.fromRGB(124, 124, 124)
     text.TextSize = 10.000
     text.TextTransparency = 1
@@ -3594,4 +3594,5 @@ function library:Init(key)
     return TabLibrary
 end
 return library
+
 
